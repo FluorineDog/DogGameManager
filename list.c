a265e0783a806faa86f2a24bb5f71f443e999e03
MikeDog <576268911@qq.com>
2015-11-14 09:52:31 +0800

    updata

--- list.c
+++ list.c
@@ -1,33 +1,40 @@
 #include "list.h"
-struct list_node{
-	list_node *m_prev, *m_next;	
-	void *m_pdata;	//Block of data for universial usage
-}; 
 
-struct list{
-	int m_size;
-	list_node m_head, m_tail;
-	
-};
-
-void list_initialize(list *plist){
+void list_initialize(list *plist, destructor_t destructor){
 	plist->size = 0;
 	plist->m_head.m_next = &plist->m_tail;
 	plist->m_tail.m_prev = &plist->m_head;
-}
-void list_insert(list *plist, void* pdata){
-	//always insert to the head of list
-	smart_malloc(sizeof())
-}
-void list_delete(list *plist, list_node* pwhere){
-	pwhere->m_prev->m_next = pwhere->m_next;
-	pwhere->m_next->m_prev = pwhere->m_prev;
-		
+	m_destructor =  destructor;
 }
 
-typedef bool (*cmp_t)(void *pdata1, void *pdata2);
-//to sort in the standard of cmp
-void list_sort(list *plist, cmp_t cmp);
-list_node* list_begin(list *plist){return plist->m_head.m_next;}
-list_node* list_end(list *plist){return &plist->m_tail;}
-typedef bool (*check_t)(void )
+void list_insert(list *plist, void* pdata){
+	//always insert to the tail of list
+	++size;
+	list_node *new_item = smart_malloc(sizeof(list_node));
+	new_item->m_pdata = pdata;
+	new_item->m_next = list_end(plist);
+	new_item->m_prev = list_end(plist)->prev;
+	new_item->m_next->m_prev = new_item->m_prev;
+	new_item->m_prev->m_next = new_item->m_next;
+}
+
+void list_erase(list *plist, list_node* pwhere){
+	--size;
+	pwhere->m_prev->m_next = pwhere->m_next;
+	pwhere->m_next->m_prev = pwhere->m_prev;
+	plist->m_destructor(pwhere->mdata);
+}
+
+//to sort in the standard of cmp with method of MergeSort
+void list_sort(list *plist, cmp_t cmp){
+	
+}
+list_node* list_find(list *plist, check_t check){
+	for(list_node* iter = list_begin(plist);\
+	iter!= list_end(plist);iter = iter-m_next){
+		if(check(iter->m_pdata)){
+			return iter;
+		}
+	}
+	return iter;
+}
--- list.h
+++ list.h
@@ -1,29 +1,36 @@
+//This file provide a framework of common lists
+//You must specify it before any usage
 #ifndef DOG_LIST_H_
 #define DOG_LIST_H_
+#include "gc.h"
 
-
-struct list_node{
+struct list_node_{
 	list_node *m_prev, *m_next;	
 	void *m_pdata;	//Block of data for universial usage
 }; 
-typedef constructor_t 
-
-struct list{
+typedef struct list_node_ list_node;
+typedef void (*destructor_t)(void * data);
+struct list_{
 	int m_size;
 	list_node m_head, m_tail;
+	destructor_t m_destructor;//for m_pdata
 };
+typedef struct list_ list;
 
-void list_initialize(list *plist);
+void list_initialize(list *plist, destructor_t destructor);
 void list_insert(list *plist, void* pdata);
-void list_delete(list *plist, list_node* pwhere);
+void list_erase(list *plist, list_node* pwhere);
 
 typedef bool (*cmp_t)(void *pdata1, void *pdata2);
 //to sort in the standard of cmp
 void list_sort(list *plist, cmp_t cmp);
+
+typedef bool (*check_t)(void* pdata);
+//to find the first matched item
+list_node* list_find(list *plist, check_t check);
+
 list_node* list_begin(list *plist){return plist->m_head.m_next;}
 list_node* list_end(list *plist){return &plist->m_tail;}
-typedef bool (*check_t)(void* pdata){
-	return pdata;
-}
+
 
 #endif //DOG_LIST_H_
